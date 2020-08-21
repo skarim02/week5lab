9204ed56121b066b9af7e6a9f90f362b11cff3f8
skarim02 <shahzad.karim@gmail.com>
2020-08-21 14:50:13 -0400

    trying to do rebase

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
+import datetime
+
+print (datetime.datetime.now())
+
 print ('foo')
 print ('hello')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
-print ('foo')
 print ('foo')
 print ('foo')
 print ('foo')
