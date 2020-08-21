1807f7562e5235f52631986f1f7e8ee55d8f76aa
skarim02 <shahzad.karim@gmail.com>
2020-08-21 16:11:43 -0400

    adding changes to master

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-print ('stuff')
-print ('stuff')
-print ('stuff')
-print ('stuff')
 import datetime
 print (datetime.datetime.now())
-print ('foo')
-print ('hello')
-print ('foo')
-print ('foo')
-print ('foo')
+print ('hi there, what\'s going on')
