49dd5bd0e61cbefd836a3f152e69a53f02112d94
skarim02 <shahzad.karim@gmail.com>
2020-08-21 14:43:39 -0400

    Revert "adding stuff"
    This reverts commit f09f150a6faa5a5343f4ec28af893138c3a8627f.

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-import datetime
-
-print (datetime.datetime.now())
+print ('foo')
+print ('hello')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
+print ('foo')
 print ('foo')
