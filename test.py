f51bc27d2a8d91b96513659b3ebce07ffccbf427
skarim02 <shahzad.karim@gmail.com>
2020-08-24 08:14:22 -0400

    fix rebase continue

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import datetime
+import os
+
+print (os.path.sys.path)
 print (datetime.datetime.now())
+<<<<<<< HEAD
 print ('\'s going on')
 print ('\'s going on')
 print ('\'s going on')
@@ -10,4 +14,7 @@ print ('\'s going on')
 print ('\'s going on')
 print ('\'s going on')
 
+=======
+print ('hi there, what\'s going on')
+>>>>>>> ad84acf... modified the file
 
