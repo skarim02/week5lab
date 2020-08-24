9f5c7d2adfefe5f352e95106497b68475764f4a8
skarim02 <shahzad.karim@gmail.com>
2020-08-24 08:51:52 -0400

    new commit for repo

--- test.py
+++ test.py
@@ -3,18 +3,4 @@ import os
 
 print (os.path.sys.path)
 print (datetime.datetime.now())
-<<<<<<< HEAD
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-print ('\'s going on')
-
-=======
-print ('hi there, what\'s going on')
->>>>>>> ad84acf... modified the file
 
